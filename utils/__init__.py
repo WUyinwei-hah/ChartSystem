848f9c9249a177f41f92b4113fc27d6f7ffc7b0a
Obsession <6535155+W3276027@user.noreply.gitee.com>
2022-05-08 10:21:29 +0800

    添加了声明

---
+++ utils/__init__.py
@@ -0,0 +1,7 @@
+"""
+@Project: 软件构造课设
+@Description: chart的数学工具模块
+@Time:2022/4/22
+@Author:WYW
+
+"""
---
+++ widgets/__init__.py
@@ -0,0 +1,7 @@
+"""
+@Project: 软件构造课设
+@Description: 本程序所有的窗体组件
+@Time:2022/4/22
+@Author:WYW
+
+"""
